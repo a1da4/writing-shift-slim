8a5ee48226edf0d831f88051519fb39418cf274e
a1da4 <ai.the.taichi@gmail.com>
2024-03-07 15:57:51 +0900

    fix filename, variablename

--- scripts/run_check_target_frequency.sh
+++ scripts/run_check_target_frequency.sh
@@ -4,8 +4,8 @@ if [ $# -lt 3 ]; then
 fi
 
 target_word_list="${!#}"
-target_word_list_file=$(basename "$target_word_list")
 target_word_list_abspath=$(readlink -f "$target_word_list")
+target_word_list_filename=$(basename "$target_word_list")
 
 for ((i = 1; i <= $# - 1; i++)); do
     document="${!i}"
@@ -14,7 +14,8 @@ for ((i = 1; i <= $# - 1; i++)); do
     echo "processing...: $document_filename"
 
     docker run --name writing_shift_py \
-        -w /app -v "${document_abspath}":/data/"${document_filename}" -v "${target_word_list_abspath}":/data/"${target_word_list_file}" \
+        -w /app -v "${document_abspath}":/data/"${document_filename}" \
+        -v "${target_word_list_abspath}":/data/"${target_word_list_filename}" \
         ws-slim/v0 \
         python check_target_frequency.py \
             --file_pathes /data/$document_filename \
