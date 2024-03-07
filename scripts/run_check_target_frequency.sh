884fcada931008014445647fd8bf66fef9afab7f
a1da4 <ai.the.taichi@gmail.com>
2024-03-07 17:31:49 +0900

    fix avoid to conflict

--- scripts/run_check_target_frequency.sh
+++ scripts/run_check_target_frequency.sh
@@ -1,5 +1,5 @@
 if [ $# -lt 3 ]; then
-    echo "Missing arguments! Usage: <doc1> <doc2> ... <word_list>"
+    echo "Missing arguments! Usage: <doc1> <doc2> ... <docN> <word_list>"
     exit 1
 fi
 
@@ -8,28 +8,6 @@ mkdir results
 target_word_list="${!#}"
 target_word_list_abspath=$(readlink -f "$target_word_list")
 target_word_list_filename=$(basename "$target_word_list")
-
-<<com
-for ((i = 1; i <= $# - 1; i++)); do
-    document="${!i}"
-    document_abspath=$(readlink -f "$document")
-    document_filename=$(basename "$document")
-    echo "processing...: $document_filename"
-
-    docker run --name ws-slim_container \
-        -w /app -v "${document_abspath}":/data/"${document_filename}" \
-        -v "${target_word_list_abspath}":/data/"${target_word_list_filename}" \
-        -v ./results:/results \
-        ws-slim/v1 \
-        python check_target_frequency.py \
-            --file_pathes /data/$document_filename \
-            --target_words /data/$target_word_list_filename
-
-    docker remove ws-slim_container
-
-done
-
-com
 
 document_commands=""
 document_filepathes=""
@@ -46,7 +24,7 @@ for ((i = 1; i <= $# - 1; i++)); do
 
 done
 
-docker run --name ws-slim_container \
+docker run --name ws-slim_container_check \
     -w /app ${document_commands} \
     -v "${target_word_list_abspath}":/data/"${target_word_list_filename}" \
     -v ./results:/results \
@@ -55,5 +33,5 @@ docker run --name ws-slim_container \
         --file_pathes ${document_filepathes} \
         --target_words /data/${target_word_list_filename}
 
-docker remove ws-slim_container
+docker remove ws-slim_container_check
 
