32c2e0c49c0a7a537907ef30716483c3a16fdb42
a1da4 <ai.the.taichi@gmail.com>
2024-03-21 11:52:15 +0900

    fix arguments

--- scripts/run_understand_neighbors.sh
+++ scripts/run_understand_neighbors.sh
@@ -1,11 +1,12 @@
-if [ $# -lt 4 ]; then
-    echo "Missing arguments! Usage: <window> <dimension> <word_list> <topk>"
+if [ $# -lt 3 ]; then
+    echo "Missing arguments! Usage: <joint_vector> <word_list> <topk>"
+    echo " <joint_vector>: please check ./results/models/ directory"
     exit 1
 fi
 
 
-window=${@: -4:1}
-dimension=${@: -3:1}
+joint_vector=${@: -3:1}
+joint_vector_filename=$(basename "$joint_vector")
 target_word_list=${@: -2:1}
 target_word_list_abspath=$(readlink -f "$target_word_list")
 target_word_list_filename=$(basename "$target_word_list")
@@ -18,7 +19,7 @@ docker run --name ws-slim_container_understand \
     ws-slim \
     python calculate_neighbors.py \
         --pickle_id2word /results/id2word.pkl \
-        --joint_vector /results/models/WV_joint_d-${dimension}_w-${window}_s-1.npy \
+        --joint_vector /results/models/${joint_vector_filename} \
         --target_words /data/${target_word_list_filename} \
         --topk 20
 
